4939824972fed24101e4a2c8d44664aa9be0d646
yquintero.andes <yquintero.andes@gmail.com>
2024-06-13 21:54:28 -0500

    cambio ruta a put

--- routes/cliente.js
+++ routes/cliente.js
@@ -7,8 +7,8 @@ const clienteController = require('../controllers/clienteController');
 router.post('/', clienteController.agregarClientes);
 router.get('/', clienteController.mostrarClientes);
 router.get('/:id', clienteController.buscarCliente);
-//router.put('/:id', clienteController.actualizarClientes);
-router.patch('/:id', clienteController.modificarClientes);
+router.put('/:id', clienteController.actualizarClientes);
+//router.patch('/:id', clienteController.modificarClientes);
 router.delete('/:id', clienteController.eliminarClientes);
 
 
